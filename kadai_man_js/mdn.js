97c255e87d8e0ea20e80882d7ff77c44e63b1552
sentluisejoy <144322513+sentluisejoy@users.noreply.github.com>
2023-12-04 12:48:53 +0900

    kadai_man_jsをアップロード

---
+++ kadai_man_js/mdn.js
@@ -0,0 +1,12 @@
+//const event = new Date(2023, 8, 22);
+//console.log(event.toDateString());
+
+//const Date = {years: '2023', month: '8', day: '22'};
+//console.log(Date.years + "年" + Date.month + "月" + Date.day + "日");
+
+
+const moonLanding = new Date('Septenber 22, 2023');
+console.log(moonLanding.getFullYear() + "年" + moonLanding.getMonth() + "月" + moonLanding.getDate()+ "日");
+//console.log(moonLanding.getFullYear() + "年");
+//console.log(moonLanding.getMonth() +"月");
+//console.log(moonLanding.getDate()+ "日");
